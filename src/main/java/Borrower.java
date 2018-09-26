8675d40e17f7ad4f4bef435fda16d346b51698e4
Shannon Holmes <snholmes13@gmail.com>
2018-09-26 01:08:25 +0100

    add info to Borrower and BorrowerTest classes

--- src/main/java/Borrower.java
+++ src/main/java/Borrower.java
@@ -1,2 +1,11 @@
 public class Borrower {
+    private String name;
+
+    public Borrower(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/test/java/BorrowerTest.java
+++ src/test/java/BorrowerTest.java
@@ -1,2 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BorrowerTest {
+    Borrower borrower;
+
+    @Before
+    public void before(){
+        borrower = new Borrower("Ruby");
+    }
+
+    @Test
+    public void borrowerHasName() {
+        assertEquals("Ruby", borrower.getName());
+    }
 }
